bb1e847c73118ef9b52bcb9661241eee36ebddbc
Rohit <rv17837@outlook.com>
2023-01-13 21:16:02 +0530

    backtracking

---
+++ basic.java
@@ -0,0 +1,26 @@
+public class basic{
+    public static void changeArr(int arr[], int i, int val){
+        //base case
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+        //recurrsion
+        arr[i] = val;
+        changeArr(arr,i+1,val+1);
+        arr[i] -= 4;
+    }
+    //Method to print elements of array
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = new int[5];
+        changeArr(arr,0,1);
+        printArr(arr);
+
+    }
+}
---
+++ subSets.java
@@ -0,0 +1,23 @@
+public class subSets{
+    public static void findSubsets(String str, String ans, int i){
+        //Base case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        //Recurrsion
+        //if yes
+        findSubsets(str,ans+str.charAt(i),i+1);
+        //if no
+        findSubsets(str,ans,i+1);
+    }
+    public static void main(String args[]){
+        String str = "abc";
+        findSubsets(str,"",0);
+    }
+}
